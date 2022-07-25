5c4a40db56926772e41d61b5d03f4741c22dc33c
mlc217 <mlc217@yahoo.com>
2022-07-25 17:24:40 -0400

    new text added

--- src/test/resources/Queries/day02/MultiRowFunctions.sql
+++ src/test/resources/Queries/day02/MultiRowFunctions.sql
@@ -10,6 +10,23 @@ select count (distinct FIRST_NAME) from EMPLOYEES;
 select count(*) from EMPLOYEES
 where JOB_ID in ('IT_PROG', 'SA_REP');
 
+select count (*) from EMPLOYEES
+where DEPARTMENT_ID is null;
+
+
+select count(DEPARTMENT_ID) from EMPLOYEES
+where DEPARTMENT_ID is null ;
+
+select count(*) from EMPLOYEES
+where DEPARTMENT_ID is not null ;
+
+
+select max(SALARY) from EMPLOYEES;
+
+select min(SALARY) from EMPLOYEES;
+
+
+
 
 
 
