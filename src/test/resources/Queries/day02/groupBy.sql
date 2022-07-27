9e977efd4ddc2ddfe10aa930b98f3b4db124f0a2
mlc217 <mlc217@yahoo.com>
2022-07-26 22:55:40 -0400

    new text

--- src/test/resources/Queries/day02/groupBy.sql
+++ src/test/resources/Queries/day02/groupBy.sql
@@ -27,6 +27,26 @@ where DEPARTMENT_ID is not null
 group by DEPARTMENT_ID
 order by DEPARTMENT_ID;
 
+select LOCATION_ID,count(*)  from DEPARTMENTS
+group by LOCATION_ID;
+
+select COUNTRY_ID, count(*)from LOCATIONS
+group by COUNTRY_ID
+order by COUNTRY_ID asc;
+
+
+select REGION_ID, count(*) from COUNTRIES
+group by REGION_ID
+order by REGION_ID;
+
+
+select MANAGER_ID, count(*)  from EMPLOYEES
+where MANAGER_ID is not null
+group by MANAGER_ID
+having count(*)>5;
+
+
+
 
 
 
