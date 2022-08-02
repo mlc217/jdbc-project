4f9eee1b1e642935d7b3b0d1baa3ae9ce7ea9a93
mlc217 <mlc217@yahoo.com>
2022-08-02 18:48:22 -0400

    new data added

--- src/test/resources/Queries/day04/aliases_singleRow.sql
+++ src/test/resources/Queries/day04/aliases_singleRow.sql
@@ -8,3 +8,35 @@ select FIRST_NAME from EMPLOYEES;
 
 
 select max(SALARY) as highest_salary from EMPLOYEES;
+
+select FIRST_NAME as "given_name" from EMPLOYEES;
+
+select EMAIL||'gmail.com' as  full_email from EMPLOYEES;
+
+select concat(EMAIL,'@gmail.com') as full_email from EMPLOYEES;
+
+
+select lower(concat(EMAIL,'@gmail.com')) as full_email from EMPLOYEES;
+
+select upper(concat(EMAIL,'@gmail.com')) as full_email from EMPLOYEES;
+
+
+select EMAIL from EMPLOYEES;
+select initcap(EMAIL) from EMPLOYEES;
+
+select length(EMAIL) from EMPLOYEES;
+
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by length(FIRST_NAME);
+
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by length_firstname;
+
+select FIRST_NAME,length(FIRST_NAME) as length_firstname from EMPLOYEES
+order by 2;
+
+select substr(FIRST_NAME,0,1)|| '.'||substr(LAST_NAME,0,1)|| '.' from EMPLOYEES;
+
+select substr(FIRST_NAME,1,1)|| '.'||substr(LAST_NAME,1,1)|| '.' from EMPLOYEES;
+
+
